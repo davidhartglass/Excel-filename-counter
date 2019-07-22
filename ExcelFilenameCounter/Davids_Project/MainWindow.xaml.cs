d6ba4e74cf27327e7deef6a4245c8f2b98b6c287
David Hartglass <36768373+davidhartglass@users.noreply.github.com>
2019-07-22 17:57:52 -0400

    Add files via upload

--- ExcelFilenameCounter/Davids_Project/MainWindow.xaml.cs
+++ ExcelFilenameCounter/Davids_Project/MainWindow.xaml.cs
@@ -27,12 +27,7 @@ namespace Davids_Project
     public partial class MainWindow : Window
     {
         private string myFile;
-        private List<string> fileNameList;
-        private int count;
         Dictionary<string, string> myDictionary = new Dictionary<string, string>();
-        DataGridColumn filenameDataGridColumn = new DataGridTextColumn();
-        DataGridColumn countFilesDataGridColumn = new DataGridTextColumn();
-
 
         public MainWindow()
         {
@@ -78,102 +73,16 @@ namespace Davids_Project
             await Task.Run(() => getExcelData());
         }
 
-        private void getExcelData()
+        private async void getExcelData()
         {
+            Excel_Integration myExcelIntegration = new Excel_Integration();
+
             this.Dispatcher.Invoke(() =>
             {
                 goButton.IsEnabled = false;
             });
 
-            //Create COM Objects. Create a COM object for everything that is referenced
-            bool excelWasRunning = System.Diagnostics.Process.GetProcessesByName("excel").Length > 0;
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(myFile);
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-
-            int rowCount = xlRange.Rows.Count;
-            int colCount = xlRange.Columns.Count;
-
-            fileNameList = new List<string>();
-
-            try
-            {
-                //iterate over the rows and columns and print to the console as it appears in the file
-                //excel is not zero based!!
-                for (int i = 1; i <= rowCount; i++)
-                {
-                    for (int j = 1; j <= colCount; j++)
-                    {
-                        //new line
-                        if (j == 1)
-                            Console.Write("\r\n");
-
-                        //write the value to the console
-                        if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                            Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-
-                        else if (xlRange.Cells[i, j].Value2.ToString() == "FriendlyName")
-                        {
-                            break;
-                        }
-
-                        if (xlRange.Cells[i, j].Value2.ToString() != null)
-                        {
-                            fileNameList.Add(xlRange.Cells[i, j].Value2.ToString());
-                        }
-
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                //cleanup
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-
-                //release com objects to fully kill excel process from running in the background
-                Marshal.ReleaseComObject(xlRange);
-                Marshal.ReleaseComObject(xlWorksheet);
-
-                if (xlApp != null) xlWorkbook.Close();
-                if (xlApp != null) xlApp.Quit();
-
-                xlApp = null;
-            }
-
-            var g = fileNameList.GroupBy(i => i);
-
-            foreach (var grp in g)
-            {
-                Console.WriteLine("{0} {1}", grp.Key, grp.Count());
-                myDictionary.Add(grp.Key, grp.Count().ToString());
-            }
-
-            this.Dispatcher.Invoke(() =>
-            {
-                myGrid.ItemsSource = myDictionary;
-            });
-
-            //myGrid.Items.Add(myDictionary);
-
-            //cleanup
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            //release com objects to fully kill excel process from running in the background
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
-
-            xlWorkbook.Close(true);
-
-            xlApp = null;
-            Marshal.ReleaseComObject(xlWorkbook);
-
-            //quit and release
-            if (xlApp != null) { xlApp.Quit(); }
+            await Task.Run(() => myDictionary = myExcelIntegration.getExcelData(myFile));
 
             this.Dispatcher.Invoke(() =>
             {
